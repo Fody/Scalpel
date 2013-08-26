1f065eef634263151b721b6f33abc1c60982d6b4
Simon Cropp <simon.cropp@gmail.com>
2013-08-26 23:00:07 +1000

    refs

--- AssemblyToProcess/ForceReferences.cs
+++ AssemblyToProcess/ForceReferences.cs
@@ -7,7 +7,7 @@ public class ForceReferences
 {
 #pragma warning disable 169
     Type moqType = typeof(Moq.Mock);
-    Type nsubType = typeof (NSubstitute.Arg);
+    Type nSubstituteType = typeof(NSubstitute.Arg);
     Type rhinoType = typeof (Rhino.Mocks.RhinoMocks);
     Type fakeItEasyType = typeof(A);
 #pragma warning restore 169
