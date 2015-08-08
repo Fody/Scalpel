62ac5b4a6c88e5c221c8fe4b73b9a92955db4656
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 14:42:21 +1000

    make nuget consistent

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Scalpel")]
 [assembly: AssemblyProduct("Scalpel")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
